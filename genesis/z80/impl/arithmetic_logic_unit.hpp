9d6d03dab928a195bdc0277bb712b341265245cb
Dmitry Yalin <yalin.dmitri@gmail.com>
2023-01-15 15:17:11 +0100

    Finish add instruction

--- genesis/z80/impl/arithmetic_logic_unit.hpp
+++ genesis/z80/impl/arithmetic_logic_unit.hpp
@@ -32,40 +32,104 @@ private:
 
 		switch (op)
 		{
-		case 0x86:
-			regs.main_set.A += mem.read<std::uint8_t>(regs.main_set.HL);
-			break;
 		case 0x87:
-			regs.main_set.A += regs.main_set.A;
+			add(regs, regs.main_set.A);
 			break;
 		case 0x80:
-			regs.main_set.A += regs.main_set.B;
+			add(regs, regs.main_set.B);
 			break;
 		case 0x81:
-			regs.main_set.A += regs.main_set.C;
+			add(regs, regs.main_set.C);
 			break;
 		case 0x82:
-			regs.main_set.A += regs.main_set.D;
+			add(regs, regs.main_set.D);
 			break;
 		case 0x83:
-			regs.main_set.A += regs.main_set.E;
+			add(regs, regs.main_set.E);
 			break;
 		case 0x84:
-			regs.main_set.A += regs.main_set.F;
+			add(regs, regs.main_set.H);
 			break;
 		case 0x85:
-			regs.main_set.A += regs.main_set.L;
+			add(regs, regs.main_set.L);
 			break;
-		case 0x88:
-
+		case 0xC6:
+			opcode_size = 2;
+			add(regs, mem.read<z80::opcode>(regs.PC + 1));
 			break;
+		case 0x86:
+			add(regs, mem.read<std::uint8_t>(regs.main_set.HL));
+			break;
+		case 0xDD:
+		{
+			opcode_size = 2;
+			z80::opcode op2 = mem.read<z80::opcode>(regs.PC + 1);
+			switch (op2)
+			{
+			case 0x86:
+			{
+				opcode_size = 3;
+				z80::opcode d = mem.read<z80::opcode>(regs.PC + 2);
+				add(regs, mem.read<z80::opcode>(regs.IX + d));
+				break;
+			}
+			
+			default:
+				return false;
+			}
+			break;
+		}
+		case 0xFD:
+		{
+			opcode_size = 2;
+			z80::opcode op2 = mem.read<z80::opcode>(regs.PC + 1);
+			switch (op2)
+			{
+			case 0x86:
+			{
+				opcode_size = 3;
+				z80::opcode d = mem.read<z80::opcode>(regs.PC + 2);
+				add(regs, mem.read<z80::opcode>(regs.IY + d));
+				break;
+			}
+			
+			default:
+				return false;
+			}
+			break;
+		}
 		default:
 			return false;
 		}
 
+		regs.main_set.flags.S = (regs.main_set.A < 0) ? 1 : 0;
+		regs.main_set.flags.Z = (regs.main_set.A == 0) ? 1 : 0;
+		regs.main_set.flags.N = 0;
+
 		regs.PC += opcode_size;
 
 		return true;
+	}
+
+protected:
+	/* 8-Bit Arithmetic Group */
+	inline static void add(z80::cpu_registers& regs, std::int8_t b)
+	{
+		std::uint8_t _a = (std::uint8_t)regs.main_set.A;
+		std::uint8_t _b = (std::uint8_t)b;
+
+		auto& flags = regs.main_set.flags;
+
+		// check Half Carry Flag (H)
+		flags.H = ((_a & 0x0f) + (_b & 0x0f) > 0xf) ? 1 : 0;
+
+		// check Parity/Overflow Flag (P/V)
+		flags.PV = (int)_a + (int)_b > 127 || (int)_a + (int)b < -128 ? 1 : 0;
+
+		// check Carry Flag (C)
+		flags.C = (int)_a + (int)_b > 0xff ? 1 : 0;
+
+		regs.main_set.A += b;
 	}
 };
 
