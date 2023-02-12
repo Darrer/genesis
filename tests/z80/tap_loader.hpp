869c90f8bfb40d4b048e9dc21d8bf71440adead1
Dmitry Yalin <yalin.dmitri@gmail.com>
2023-02-12 10:24:36 +0100

    Integrate more tests

---
+++ tests/z80/tap_loader.hpp
@@ -0,0 +1,178 @@
+#ifndef __TAP_LOADER_HPP__
+#define __TAP_LOADER_HPP__
+
+#include <vector>
+#include <cstdint>
+#include <string_view>
+#include <fstream>
+#include <iterator>
+
+#include "z80/cpu.h"
+#include "string_utils.hpp"
+
+namespace test::z80
+{
+
+enum tap_flag : std::uint8_t
+{
+	header = 0x00,
+	data = 0xFF,
+};
+
+class tap_block
+{
+public:
+	tap_flag flag;
+	std::vector<std::uint8_t> data;
+	std::uint8_t checksum;
+};
+
+
+enum header_type : std::uint8_t
+{
+	program,
+	number_array,
+	character_array,
+	code_file
+};
+
+class tap_header
+{
+public:
+	tap_header(const tap_block& block)
+	{
+		if(block.flag != tap_flag::header)
+		{
+			throw std::runtime_error("tap_header error: expected header tap flag");
+		}
+
+		if(block.data.size() != 17)
+		{
+			throw std::runtime_error("tap_header error: unexpected header length");
+		}
+
+		type = (header_type)block.data[0];
+		std::copy(block.data.cbegin() + 1, block.data.cbegin() + 11, std::back_inserter(filename));
+		
+		assign_16(length, block.data[11], block.data[12]);
+		assign_16(param1, block.data[13], block.data[14]);
+		assign_16(param2, block.data[15], block.data.at(16));
+	}
+
+	header_type type;
+	std::string filename;
+	std::uint16_t length;
+	std::uint16_t param1;
+	std::uint16_t param2;
+
+private:
+	void assign_16(std::uint16_t& dest, std::uint8_t low, std::uint8_t high)
+	{
+		dest = (high << 8) | low;
+	}
+};
+
+
+std::vector<tap_block> read_tap(std::string tap_path)
+{
+	std::ifstream fs(tap_path, std::ios_base::binary);
+	if (!fs.is_open())
+	{
+		throw std::runtime_error("read_tap error: failed to open file '" + tap_path + "'");
+	}
+
+	std::vector<tap_block> res;
+
+	// TAP files in little-endian - don't need to convert
+
+	while (fs)
+	{
+		std::uint16_t block_size = 0;
+		if(!fs.read(reinterpret_cast<char*>(&block_size), sizeof(block_size)))
+		{
+			// nothing left to read
+			break;
+		}
+
+		if(block_size <= 2)
+		{
+			throw std::runtime_error("read_tap error: block size is too small (should be at least 3 bits long)");
+		}
+
+		tap_block block;
+
+		if(!fs.read(reinterpret_cast<char*>(&block.flag), sizeof(block.flag)))
+		{
+			throw std::runtime_error("read_tap error: corrupted file (cannot read flag)");
+		}
+
+		// 1 byte for flag; 1 byte for checksum
+		const std::uint16_t data_size = block_size - 2;
+		for(std::uint16_t i = 0; i < data_size; ++i)
+		{
+			char c;
+			if(!fs.get(c))
+			{
+				throw std::runtime_error("read_tap error: corrupted file (cannot read data " + su::hex_str(data_size) + ")");
+			}
+
+			block.data.push_back(c);
+		}
+
+		block.data.shrink_to_fit();
+
+		if(!fs.read(reinterpret_cast<char*>(&block.checksum), sizeof(block.checksum)))
+		{
+			throw std::runtime_error("read_tap error: corrupted file (cannot read checksum)");
+		}
+
+		res.push_back(std::move(block));
+	}
+
+	res.shrink_to_fit();
+	return res;
+}
+
+
+void load_tap(std::string tap_path, genesis::z80::cpu& cpu)
+{
+	auto blocks = read_tap(tap_path);
+
+	for(std::size_t i = 0; i < blocks.size(); ++i)
+	{
+		auto& block = blocks[i];
+		if(block.flag == tap_flag::header)
+		{
+			tap_header header(block);
+
+			if(i + 1 < blocks.size())
+			{
+				auto& next_block = blocks[i + 1];
+				if(next_block.flag == tap_flag::data)
+				{
+					// load only code file, 've got no idea what to do with the other types...
+					if(header.type == header_type::code_file)
+					{
+						auto& mem = cpu.memory();
+						genesis::z80::memory::address base = header.param1;
+						for(std::size_t j = 0; j < next_block.data.size(); ++j)
+						{
+							mem.write(base + j, next_block.data[j]);
+						}
+
+						cpu.registers().PC = base;
+						cpu.registers().SP = (std::int16_t)0xFF00;
+
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	throw std::runtime_error("load_tap error: failed to load tap file (not found header or code file data)");
+}
+
+}
+
+#endif // __TAP_LOADER_HPP__
---
+++ tests/z80/tests_runner.cpp
@@ -0,0 +1,205 @@
+#include <string_view>
+#include <filesystem>
+#include <fstream>
+#include <gtest/gtest.h>
+
+#include "../helper.hpp"
+#include "tap_loader.hpp"
+
+#include "z80/cpu.h"
+#include "z80/io_ports.hpp"
+#include "string_utils.hpp"
+
+using namespace genesis;
+
+class test_io_ports : public z80::io_ports
+{
+public:
+	std::uint8_t in(std::uint8_t /*dev*/, std::uint8_t /*param*/) override
+	{
+		return 0xBF;
+	}
+
+	void out(std::uint8_t dev, std::uint8_t /*param*/, std::uint8_t data) override
+	{
+		if(dev != 0x1) return;
+
+		data = map_char(data);
+
+		if(!need_to_print(data))
+			return;
+
+		if(data == 13) 
+			data = '\n';
+
+		if(data == '\n')
+			parse_line();
+
+		one_line << data;
+		
+		check_terminated();
+		if(data == '\n')
+		{
+			std::cout << one_line.str();
+			one_line.str(std::string());
+		}
+	}
+
+	bool terminated()
+	{
+		return _terminated;
+	}
+
+	int succeded() { return num_succeded; }
+	int failed() { return num_failed; }
+
+
+private:
+	bool need_to_print(char ch)
+	{
+		return isprint(ch) || ch == '\n';
+	}
+
+	char map_char(char ch)
+	{
+		switch (ch)
+		{
+		case '\r':
+			return '\n';
+
+		case 30: // record separator
+		case 26: // substitute 
+			return ' ';
+
+		default:
+			return ch;
+		}
+	}
+
+	void check_terminated()
+	{
+		if(one_line.str().starts_with("Tests complete"))
+			_terminated = true;
+
+		if(one_line.str().starts_with("Result:"))
+			_terminated = true;
+	}
+
+	void parse_line()
+	{
+		std::string str = one_line.str();
+		std::transform(str.cbegin(), str.cend(), str.begin(), [](char ch) { return tolower(ch); } );
+		
+		if(str.ends_with("ok"))
+			++num_succeded;
+		
+		if(str.find("expected") != std::string::npos)
+			++num_failed;
+	}
+
+private:
+	std::stringstream one_line;
+	bool _terminated = false;
+	int num_succeded = 0;
+	int num_failed = 0;
+};
+
+
+void report_results(int cycles, int num_succeded, int num_failed)
+{
+	std::cout << "Z80 Test complete, succeeded: " << num_succeded << ", failed: " << num_failed
+		<< ", cycles: " << cycles << std::endl;
+}
+
+void run_test(z80::cpu& cpu, const int expected_total_tests)
+{
+	unsigned long long cycles = 0;
+	auto& ports =  static_cast<test_io_ports&>(cpu.io_ports());
+
+	while(!ports.terminated())
+	{
+		try
+		{
+			cpu.execute_one();
+			++cycles;
+		}
+		catch(...)
+		{
+			report_results(cycles, ports.succeded(), ports.failed());
+			throw;
+		}
+	}
+
+	report_results(cycles, ports.succeded(), ports.failed());
+
+	ASSERT_EQ(0, ports.failed()) << "Some tests were failed!";
+	ASSERT_EQ(expected_total_tests, ports.succeded()) << "Some tests were skipped";
+}
+
+void patch_mem(z80::memory& mem)
+{
+	mem.write(0x1601, 0xC9); // RET
+
+	mem.write(0x10, 0x01D3); // OUT (1), A
+	mem.write(0x12, 0xC9);   // RET
+}
+
+void load_zex(z80::memory& mem, z80::memory::address base, const std::string& bin_path)
+{
+	std::ifstream fs(bin_path, std::ios_base::binary);
+	if (!fs.is_open())
+	{
+		throw std::runtime_error("load_zex error: failed to open file '" + bin_path + "'");
+	}
+
+	z80::memory::address offset = 0x0;
+	while (fs)
+	{
+		char c;
+		if (fs.get(c))
+		{
+			mem.write(base + offset, c);
+			++offset;
+		}
+	}
+}
+
+void run_zex(const std::string& zex_path)
+{
+	auto cpu = z80::cpu(std::make_shared<z80::memory>(), std::make_shared<test_io_ports>());
+
+	cpu.registers().PC = 0x8000;
+	load_zex(cpu.memory(), cpu.registers().PC, zex_path);
+	patch_mem(cpu.memory());
+
+	run_test(cpu, 67);
+}
+
+void run_tap(const std::string& tap_path)
+{
+	auto cpu = z80::cpu(std::make_shared<z80::memory>(), std::make_shared<test_io_ports>());
+
+	test::z80::load_tap(tap_path, cpu);
+	patch_mem(cpu.memory());
+
+	run_test(cpu, 160);
+}
+
+
+TEST(Z80, ZEXDOC)
+{
+	auto bin_path = get_exec_path() / "z80" / "zexdoc";
+	run_zex(bin_path.string());
+}
+
+TEST(Z80, ZEXALL)
+{
+	auto bin_path = get_exec_path() / "z80" / "zexall";
+	run_zex(bin_path.string());
+}
+
+TEST(Z80, FULLTAP)
+{
+	auto tap_path = get_exec_path() / "z80" / "z80full.tap";
+	run_tap(tap_path.string());
+}
