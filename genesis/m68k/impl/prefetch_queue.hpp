75eab40d7b68c88bb54897908b49f91437b10530
Dmitry Yalin <yalin.dmitri@gmail.com>
2023-02-25 12:38:29 +0100

    Add prefetch queue

---
+++ genesis/m68k/impl/prefetch_queue.hpp
@@ -0,0 +1,103 @@
+#ifndef __M68K_PREFETCH_QUEUE_HPP__
+#define __M68K_PREFETCH_QUEUE_HPP__
+
+#include <string_view>
+
+#include "bus_manager.hpp"
+#include "m68k/cpu_registers.hpp"
+
+namespace genesis::m68k
+{
+
+class prefetch_queue
+{
+private:
+	enum fetch_state : std::uint8_t
+	{
+		IDLE,
+		FETCH_ONE,
+		FETCH_TWO
+	};
+
+public:
+	prefetch_queue(m68k::bus_manager& busm, m68k::cpu_registers& regs) : busm(busm), regs(regs)
+	{
+		IRC = IR = IRD = 0x0;
+	}
+
+	// Consider moving to cpu_registers
+	std::uint16_t IRC;
+	std::uint16_t IR;
+	std::uint16_t IRD;
+
+	bool is_idle() const
+	{
+		return state == IDLE;
+	}
+
+	void init_fetch_one()
+	{
+		assert_idle("init_fetch_one");
+
+		busm.init_read_word(regs.PC);
+		state = FETCH_ONE;
+	}
+
+	void init_fetch_two()
+	{
+		assert_idle("init_fetch_two");
+
+		busm.init_read_word(regs.PC);
+		state = FETCH_TWO;
+	}
+
+	void cycle()
+	{
+		switch (state)
+		{
+		case IDLE:
+			break;
+		
+		case FETCH_ONE:
+			if(!busm.is_idle()) break;
+			on_read_finished();
+			state = IDLE; break;
+
+		case FETCH_TWO:
+			if(!busm.is_idle()) break;
+			on_read_finished();
+			busm.init_read_word(regs.PC);
+			state = FETCH_ONE; break;
+
+		default:
+			break;
+		}
+	}
+
+private:
+	void on_read_finished()
+	{
+		IR = IRC;
+		IRC = busm.letched_word();
+		IRD = IR;
+		regs.PC += 2;
+	}
+
+	void assert_idle(std::string_view caller)
+	{
+		if(!is_idle())
+		{
+			throw std::runtime_error("prefetch_queue::" + std::string(caller) +
+				" error: cannot start new prefetch while in the middle of the other");
+		}
+	}
+
+private:
+	m68k::bus_manager& busm;
+	m68k::cpu_registers& regs;
+	fetch_state state = IDLE;
+};
+
+}
+
+#endif // __M68K_PREFETCH_QUEUE_HPP__
---
+++ tests/m68k/prefetch_queue.cpp
@@ -0,0 +1,87 @@
+#include <gtest/gtest.h>
+
+#include "m68k/impl/prefetch_queue.hpp"
+#include "m68k/impl/bus_manager.hpp"
+#include "m68k/cpu_registers.hpp"
+
+
+using namespace genesis;
+
+#define setup_test() \
+	m68k::cpu_bus bus; \
+	auto mem = std::make_shared<m68k::memory>(); \
+	m68k::bus_manager busm(bus, *mem); \
+	m68k::cpu_registers regs; \
+	m68k::prefetch_queue pq(busm, regs)
+
+
+std::uint32_t wait_idle(m68k::prefetch_queue& pq, m68k::bus_manager& busm)
+{
+	std::uint32_t cycles = 0;
+	while (!pq.is_idle() || !busm.is_idle())
+	{
+		busm.cycle();
+		pq.cycle();
+		++cycles;
+	}
+	
+	return cycles;
+}
+
+std::uint32_t fetch_one(m68k::prefetch_queue& pq, m68k::bus_manager& busm)
+{
+	pq.init_fetch_one();
+	return wait_idle(pq, busm);
+}
+
+std::uint32_t fetch_two(m68k::prefetch_queue& pq, m68k::bus_manager& busm)
+{
+	pq.init_fetch_two();
+	return wait_idle(pq, busm);
+}
+
+const std::uint32_t expected_fetch_cycles = 4;
+
+TEST(M68K_PREFETCH_QUEUE, FETCH_ONE)
+{
+	setup_test();
+
+	regs.PC = 0x101;
+	std::uint16_t val = 42240;
+	mem->write(regs.PC, val);
+
+	auto actual_fetch_cycles = fetch_one(pq, busm);
+
+	ASSERT_EQ(expected_fetch_cycles, actual_fetch_cycles);
+	ASSERT_EQ(0x103, regs.PC);
+	ASSERT_EQ(val, pq.IRC);
+}
+
+TEST(M68K_PREFETCH_QUEUE, FETCH_TWO)
+{
+	setup_test();
+
+	regs.PC = 0x101;
+	std::uint16_t val = 42240;
+	std::uint16_t val2 = 11666;
+	mem->write(regs.PC, val);
+	mem->write(regs.PC + 2, val2);
+
+	auto actual_fetch_cycles = fetch_two(pq, busm);
+
+	ASSERT_EQ(expected_fetch_cycles * 2, actual_fetch_cycles);
+	ASSERT_EQ(0x105, regs.PC);
+	ASSERT_EQ(val, pq.IRD);
+	ASSERT_EQ(val, pq.IR);
+	ASSERT_EQ(val2, pq.IRC);
+}
+
+TEST(M68K_PREFETCH_QUEUE, INTERRUPT_CYCLE_THROW)
+{
+	setup_test();
+
+	pq.init_fetch_one();
+
+	ASSERT_THROW(pq.init_fetch_one(), std::runtime_error);
+	ASSERT_THROW(pq.init_fetch_two(), std::runtime_error);
+}
