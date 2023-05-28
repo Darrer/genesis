36916ac363a0974468b1fa0f22a6c75c57f93447
Dmitry Yalin <yalin.dmitri@gmail.com>
2023-05-28 18:00:58 +0200

    Add more helper methods

--- genesis/vdp/register_set.h
+++ genesis/vdp/register_set.h
@@ -8,6 +8,7 @@
 namespace genesis::vdp
 {
 
+// c - stands for cell
 enum class plane_dimension
 {
 	c32,
@@ -16,11 +17,50 @@ enum class plane_dimension
 	invalid,
 };
 
+enum class display_height
+{
+	c30, // PAL,  240 pixels
+	c28, // NTSC, 224 pixels
+};
+
+enum class display_width
+{
+	c32, // 256 pixels
+	c40, // 320 pixels
+};
+
+enum class vertical_scrolling 
+{
+	full_screen,
+	two_cell,
+};
+
+enum class horizontal_scrolling
+{
+	full_screen,
+	line,
+	cell,
+	invalid,
+};
+
+enum class interlace_mode
+{
+	disabled,
+	normal,
+	doubled,
+};
+
 enum class dma_mode
 {
 	mem_to_vram, // M68K memory -> VRAM
 	vram_fill,
 	vram_copy,
+};
+
+enum class display_mode
+{
+	mode4, // Master System
+	mode5 // Mega Drive 
 };
 
 class register_set
@@ -119,7 +159,7 @@ public:
 	std::uint32_t window_address() const
 	{
 		std::uint32_t addr = R3.W5_1;
-		if(is_h40())
+		if(display_width() == display_width::c40)
 			addr = addr & ~1;
 		addr = addr << 11;
 		// TODO: use WA6?
@@ -129,7 +169,7 @@ public:
 	std::uint32_t sprite_address() const
 	{
 		std::uint32_t addr = R5.ST6_0;
-		if(is_h40())
+		if(display_width() == display_width::c40)
 			addr = addr & ~1;
 		addr = addr << 9;
 		// TODO: use ST7?
@@ -176,6 +216,13 @@ public:
 		}
 	}
 
+	/* DMA */
+
+	bool dma_enabled() const
+	{
+		return R1.M1 == 1;
+	}
+
 	std::uint16_t dma_length() const
 	{
 		std::uint16_t length = std::uint16_t(R20.H) << 8;
@@ -209,14 +256,93 @@ public:
 		return dma_mode::vram_copy;
 	}
 
-	bool is_h40() const // 320pixel
+	/* Interrupts */
+
+	bool vertical_interrupt_enabled() const
 	{
-		return R12.RS0 == 1;
+		return R1.IE0 == 1;
 	}
 
-	bool is_h32() const // 256pixel
+	bool horizontal_interrupt_enabled() const
 	{
-		return R12.RS0 == 0;
+		return R0.IE1 == 1;
+	}
+
+	bool external_interrupt_enabled() const
+	{
+		return R11.IE2 == 1;
+	}
+
+	/* Display settings */
+
+	display_mode display_mode() const
+	{
+		if(R1.M5 == 1)
+			return display_mode::mode5;
+		return display_mode::mode4;
+	}
+
+	display_height display_height() const
+	{
+		if(R1.M2 == 1)
+			return display_height::c30;
+		return display_height::c28;
+	}
+
+	display_width display_width() const
+	{
+		// TODO: take into accout RS1?
+		if(R12.RS0 == 1)
+			return display_width::c40;
+		return display_width::c32;
+	}
+
+	interlace_mode interlace_mode() const
+	{
+		switch (R12.LS)
+		{
+		case 0b01:
+			return interlace_mode::normal;
+		case 0b11:
+			return interlace_mode::doubled;
+		default:
+			return interlace_mode::disabled;
+		}
+	}
+
+	/* Scrolling */
+	vertical_scrolling vertical_scrolling() const
+	{
+		if(R11.VS == 1)
+			return vertical_scrolling::two_cell;
+		return vertical_scrolling::full_screen;
+	}
+
+	horizontal_scrolling horizontal_scrolling() const
+	{
+		switch (R11.HS)
+		{
+		case 0b00:
+			return horizontal_scrolling::full_screen;
+		case 0b10:
+			return horizontal_scrolling::cell;
+		case 0b11:
+			return horizontal_scrolling::line;
+
+		// TODO: invalid or line?
+		default:
+			return horizontal_scrolling::invalid;
+		}
+	}
+
+	bool freeze_hv_counter() const
+	{
+		return R0.M3 == 1;
+	}
+
+	bool in_128kb_vram_mode() const
+	{
+		return R1.VR == 1;
 	}
 
 	/* Registers */
